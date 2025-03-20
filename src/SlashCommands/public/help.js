f9f853e55eaed979b2cada2854f61c12e0ce8e48
Travis Scott <138378346+iiiMohammad@users.noreply.github.com>
2025-03-20 04:37:04 +0300

    Create help.js

---
+++ src/SlashCommands/public/help.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('عرض قائمة بجميع الأوامر مع شرحها'),
+    async execute(interaction) {
+        const commandsList = interaction.client.commands.map(command => {
+            return {
+                name: command.data.name,
+                description: command.data.description,
+            };
+        });
+      const embed = new EmbedBuilder()
+    .setTitle('قائمة الأوامر')
+    .setDescription('هذه قائمة بجميع الأوامر')
+    .setFooter({ text: `Developer by egyxo` });
+
+    commandsList.forEach(command => {
+    embed.addFields({ name: `/${command.name}`, value: command.description });
+
+});
+       
+        await interaction.reply({ embeds: [embed], ephemeral: true });
+    },
+};
